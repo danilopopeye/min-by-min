5a2b730584438342ef037c552c59d72742707460
Danilo Sousa <danilopopeye@gmail.com>
2010-12-23 08:16:11 -0200

    DRY on some nodejs stuff :)

--- mm/server.js
+++ mm/server.js
@@ -1,24 +1,20 @@
 // MM.Server
-var qs = require('querystring'),
-
-Server = module.exports = {
+var Server = module.exports = {
 	create: function(request, response) {
 		var MM = module.parent.exports;
-console.log( arguments );
-		MM.SOCKET.broadcast(
-			Server.parseURL( request.url )
-		);
+
+		MM.SOCKET.broadcast( Server.parseURL(
+			require('url').parse( request.url )
+		) );
 
 		response.writeHead( 405, { 'Content-Type': 'text/plain' });
-		response.write('Nothing to see here, move along!', 'utf8');
+		response.write('Nothing to see here, move along!\n', 'utf8');
 		response.end();
 	},
 	parseURL: function( url ){
-		var split = url.split('?');
-
 		return {
-			href: split[0],
-			params: qs.parse( split[1] || '' )
+			href: url.pathname,
+			query: require('querystring').parse( url.query || '' )
 		};
 	}
 };
